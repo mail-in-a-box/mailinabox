a50f30c411f65ceb28c7aa5ba4f1eabfa1c0c7f6
Joshua Tauberer <jt@occams.info>
2013-09-01 10:39:48 -0400

    smtp server test

---
+++ tests/smtp_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import smtplib, sys
+
+if len(sys.argv) < 3:
+        print("Usage: tests/smtp_server.py host email.to email.from")
+        sys.exit(1)
+
+host, toaddr, fromaddr = sys.argv[1:4]
+msg = """From: %s
+To: %s
+Subject: SMTP server test
+
+This is a test message.""" % (fromaddr, toaddr)
+
+server = smtplib.SMTP(host, 25)
+server.set_debuglevel(1)
+server.sendmail(fromaddr, [toaddr], msg)
+server.quit()
+
