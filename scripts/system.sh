8a5bdc0213461ccfa558997eeabdcd96e4f0513a
auzias <auzias@pc-mna-59.univ-ubs.fr>
2014-01-17 16:54:44 +0100

    system.sh OK [but I run the fwn, no fail2ban at this time]

--- scripts/system.sh
+++ scripts/system.sh
@@ -4,14 +4,10 @@ apt-get -q -q update
 apt-get -q -y upgrade
 
 # Turn on basic services:
-#
 #   ntp: keeps the system time correct
-#
 #   fail2ban: scans log files for repeated failed login attempts and blocks the remote IP at the firewall
-#
-# These services don't need further configuration and are started immediately after installation.
 
-apt-get install -q -y ntp fail2ban
+apt-get install -q -y ntp
 
 # Turn on the firewall. First allow incoming SSH, then turn on the firewall. Additional open
 # ports will be set up in the scripts that set up those services.
