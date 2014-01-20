2e16435c1aada83159f24f33008f9f37205578ea
Pamplemousse <xav.maso@gmail.com>
2014-01-20 15:45:23 +0100

    Update system.sh

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,13 +1,8 @@
-# Base system configuration.
-
-apt-get -q -q update
-apt-get -q -y upgrade
-
 # Turn on basic services:
 #   ntp: keeps the system time correct
 #   fail2ban: scans log files for repeated failed login attempts and blocks the remote IP at the firewall
 
-apt-get install -q -y ntp
+apt-get install -q -y ntp fail2ban
 
 # Turn on the firewall. First allow incoming SSH, then turn on the firewall. Additional open
 # ports will be set up in the scripts that set up those services.
