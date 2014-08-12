9f5fd6b474c53ca1d643a5251573f4be9c7b7267
jkaberg <joel@jkaberg.com>
2014-08-12 12:33:42 +0200

    fix user_backends array

--- setup/owncloud.sh
+++ setup/owncloud.sh
@@ -7,7 +7,7 @@ source /etc/mailinabox.conf # load global vars
 apt_install \
 	dbconfig-common \
 	php5-cli php5-sqlite php5-gd php5-imap php5-curl php-pear php-apc curl libapr1 libtool libcurl4-openssl-dev php-xml-parser \
-	php5 php5-dev php5-gd php5-fpm memcached php5-memcache unzip
+	php5 php5-dev php5-gd php5-fpm memcached php5-memcache unzip sqlite
 
 apt-get purge -qq -y owncloud*
 
@@ -29,15 +29,11 @@ if [ ! -f "/usr/local/lib/owncloud/config/config.php" ]; then
 
 \$CONFIG = array (
   'datadirectory' => '$STORAGE_ROOT/owncloud',
-  array (
-    0 =>
-    array (
-      'class' => 'OC_User_IMAP',
-      'arguments' =>
-      array (
-      0 => '{localhost:993/imap/ssl/novalidate-cert}',
-      ),
-    ),
+  'user_backends' => array(
+    array(
+      'class'=>'OC_User_IMAP',
+      'arguments'=>array('{localhost:993/imap/ssl/novalidate-cert}')
+    )
   ),
   "memcached_servers" => array (
     array('localhost', 11211),
