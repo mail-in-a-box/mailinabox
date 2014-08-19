9a1989357c82b0aba30beb3db1a005dbe6b21630
jkaberg <joel@jkaberg.com>
2014-08-19 13:17:13 +0200

    some makeup

--- conf/zpush/autodiscover_config.php
+++ conf/zpush/autodiscover_config.php
@@ -5,25 +5,20 @@
 * Descr     :   Autodiscover configuration file
 ************************************************/
 
-/**********************************************************************************
- *  Default settings
- */
-    // Defines the base path on the server
-    define('BASE_PATH', dirname($_SERVER['SCRIPT_FILENAME']). '/');
+// Defines the base path on the server
+define('BASE_PATH', dirname($_SERVER['SCRIPT_FILENAME']). '/');
 
-    // The Z-Push server location for the autodiscover response
-    define('SERVERURL', 'https://'.$_SERVER['SERVER_NAME'].'/Microsoft-Server-ActiveSync');
+// The Z-Push server location for the autodiscover response
+define('SERVERURL', 'https://' . $_SERVER['SERVER_NAME'] . '/Microsoft-Server-ActiveSync');
 
-    define('USE_FULLEMAIL_FOR_LOGIN', true);
+define('USE_FULLEMAIL_FOR_LOGIN', true);
 
-    define('LOGFILEDIR', '/var/log/z-push/');
-    define('LOGFILE', LOGFILEDIR . 'autodiscover.log');
-    define('LOGERRORFILE', LOGFILEDIR . 'autodiscover-error.log');
-    define('LOGLEVEL', LOGLEVEL_INFO);
-    define('LOGUSERLEVEL', LOGLEVEL);
-/**********************************************************************************
- *  Backend settings
- */
-    // the backend data provider
-    define('BACKEND_PROVIDER', 'BackendCombined');
+define('LOGFILEDIR', '/var/log/z-push/');
+define('LOGFILE', LOGFILEDIR . 'autodiscover.log');
+define('LOGERRORFILE', LOGFILEDIR . 'autodiscover-error.log');
+define('LOGLEVEL', LOGLEVEL_INFO);
+define('LOGUSERLEVEL', LOGLEVEL);
+
+// the backend data provider
+define('BACKEND_PROVIDER', 'BackendCombined');
 ?>
