2afd0be591723a6c0ea9575eaf7e1faeb6ba4ac0
Sebastian Kosch <sebastian@aldusleaf.org>
2014-08-26 12:16:20 -0400

    Replace spaces by tabs in 106-109

--- management/web_update.py
+++ management/web_update.py
@@ -103,10 +103,10 @@ def make_domain_config(domain, template, template_for_primaryhost, env):
 			for path, url in yaml.get("proxies", {}).items():
 				nginx_conf += "\tlocation %s {\n\t\tproxy_pass %s;\n\t}\n" % (path, url)
 
- 	# Add in any user customizations in the includes/ folder.
-        nginx_conf_custom_include = os.path.join(env["STORAGE_ROOT"], "www", safe_domain_name(domain) + ".conf")
-        if os.path.exists(nginx_conf_custom_include):
-                nginx_conf += "\tinclude %s;\n" % (nginx_conf_custom_include)
+	# Add in any user customizations in the includes/ folder.
+	nginx_conf_custom_include = os.path.join(env["STORAGE_ROOT"], "www", safe_domain_name(domain) + ".conf")
+	if os.path.exists(nginx_conf_custom_include):
+		nginx_conf += "\tinclude %s;\n" % (nginx_conf_custom_include)
 
 	# Ending.
 	nginx_conf += nginx_conf_parts[1]
