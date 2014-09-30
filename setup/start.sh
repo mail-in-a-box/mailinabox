d9ed25e6d8b29be448edc31e882818e8e6e882fd
H8H <mc2m382@chomeyer.de>
2014-09-30 19:50:13 +0200

    Fixed cannot change locale (en_US.UTF-8) #206

--- setup/start.sh
+++ setup/start.sh
@@ -8,6 +8,21 @@ source setup/functions.sh # load our functions
 # machine with enough memory? If not, this shows an error and exits.
 . setup/preflight.sh
 
+# Ensure Python reads/writes files in UTF-8. If the machine
+# triggers some other locale in Python, like ASCII encoding,
+# Python may not be able to read/write files. Here and in
+# the management daemon startup script.
+
+if [ -z `locale -a | grep en_US.utf8` ]; then
+    # Generate locale if not exists
+    hide_output locale-gen en_US.UTF-8
+fi
+
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
+
 # Recall the last settings used if we're running this a second time.
 if [ -f /etc/mailinabox.conf ]; then
 	# Run any system migrations before proceeding. Since this is a second run,
