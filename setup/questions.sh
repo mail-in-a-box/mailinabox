3b86b3fe66c01746512db5776073533043a16d04
Joshua Tauberer <jt@occams.info>
2015-05-19 08:37:17 -0400

    bump to email_validator 0.1.0-rc5

--- setup/questions.sh
+++ setup/questions.sh
@@ -10,7 +10,7 @@ if [ -z "$NONINTERACTIVE" ]; then
 	apt_get_quiet install dialog python3 python3-pip  || exit 1
 
 	# email_validator is repeated in setup/management.sh
-	hide_output pip3 install "email_validator==0.1.0-rc4" || exit 1
+	hide_output pip3 install "email_validator==0.1.0-rc5" || exit 1
 
 	message_box "Mail-in-a-Box Installation" \
 		"Hello and thanks for deploying a Mail-in-a-Box!
