mail-in-a-box/mailinabox

commit ec73c171c7cd2fd8f56d6342696df955c9d3f262

when installing a ssl cert for the primary hostname, dns, postfix, and dovecot all need to be updated/kicked
see https://discourse.mailinabox.email/t/there-is-a-problem-with-the-ssl-certificate/144/4

management/web_update.py
@@ -5,7 +5,7 @@
 import os, os.path, shutil, re, rtyaml
 
 from mailconfig import get_mail_domains
-from dns_update import get_custom_dns_config
+from dns_update import get_custom_dns_config, do_dns_update
 from utils import shell, safe_domain_name, sort_domains
 
 def get_web_domains(env):
@@ -237,8 +237,21 @@ def install_cert(domain, ssl_cert, ssl_chain, env):
 	os.makedirs(os.path.dirname(ssl_certificate), exist_ok=True)
 	shutil.move(fn, ssl_certificate)
 
+	ret = []
+
+	# When updating the cert for PRIMARY_HOSTNAME, also update DNS because it is
+	# used in the DANE TLSA record and restart postfix and dovecot which use
+	# that certificate.
+	if domain == env['PRIMARY_HOSTNAME']:
+		ret.append( do_dns_update(env) )
+
+		shell('check_call', ["/usr/sbin/service", "postfix", "restart"])
+		shell('check_call', ["/usr/sbin/service", "dovecot", "restart"])
+		ret.append("mail services restarted")
+
 	# Kick nginx so it sees the cert.
-	return do_web_update(env, ok_status="")
+	ret.append( do_web_update(env, ok_status="") )
+	return "\n".join(r for r in ret if r.strip() != "")
 
 def get_web_domains_info(env):
 	def check_cert(domain):

management/status_checks.py
@@ -179,9 +179,8 @@ def check_primary_hostname_dns(domain, env, dns_domains, dns_zonefiles):
 	elif tlsa25 is None:
 		env['out'].print_error("""The DANE TLSA record for incoming mail is not set. This is optional.""")
 	else:
-		env['out'].print_error("""The DANE TLSA record for incoming mail (%s) is not correct. It is '%s' but it should be '%s'. Try running tools/dns_update to
-			regenerate the record. It may take several hours for
-                        public DNS to update after a change."""
+		env['out'].print_error("""The DANE TLSA record for incoming mail (%s) is not correct. It is '%s' but it should be '%s'.
+			It may take several hours for public DNS to update after a change."""
                         % (tlsa_qname, tlsa25, tlsa25_expected))
 
 	# Check that the hostmaster@ email address exists.

commit 4fa58169f1434aa62e3f8eca7435bd2c6d03bb09

after installing an SSL certificate from the control panel the page wasn't being refreshed, broken in ec73c171c7cd2fd8f56d6342696df955c9d3f262

management/templates/ssl.html
@@ -110,7 +110,8 @@ function install_cert() {
       chain: $('#ssl_paste_chain').val()
     },
     function(status) {
-      if (status == "") {
+      if (/^OK($|\n)/.test(status)) {
+        console.log(status)
         show_modal_error("SSL Certificate Installation", "Certificate has been installed. Check that you have no connection problems to the domain.", function() { show_ssl(); $('#csr_info').slideUp(); });
       } else {
         show_modal_error("SSL Certificate Installation", status);

management/web_update.py
@@ -35,7 +35,7 @@ def get_web_domains(env):
 
 	return domains
 	
-def do_web_update(env, ok_status="web updated\n"):
+def do_web_update(env):
 	# Build an nginx configuration file.
 	nginx_conf = open(os.path.join(os.path.dirname(__file__), "../conf/nginx-top.conf")).read()
 
@@ -62,7 +62,7 @@ def do_web_update(env, ok_status="web updated\n"):
 	# enough and doesn't break any open connections.
 	shell('check_call', ["/usr/sbin/service", "nginx", "reload"])
 
-	return ok_status
+	return "web updated\n"
 
 def make_domain_config(domain, template, template_for_primaryhost, env):
 	# How will we configure this domain.
@@ -238,7 +238,7 @@ def install_cert(domain, ssl_cert, ssl_chain, env):
 	os.makedirs(os.path.dirname(ssl_certificate), exist_ok=True)
 	shutil.move(fn, ssl_certificate)
 
-	ret = []
+	ret = ["OK"]
 
 	# When updating the cert for PRIMARY_HOSTNAME, also update DNS because it is
 	# used in the DANE TLSA record and restart postfix and dovecot which use
@@ -251,8 +251,8 @@ def install_cert(domain, ssl_cert, ssl_chain, env):
 		ret.append("mail services restarted")
 
 	# Kick nginx so it sees the cert.
-	ret.append( do_web_update(env, ok_status="") )
-	return "\n".join(r for r in ret if r.strip() != "")
+	ret.append( do_web_update(env) )
+	return "\n".join(ret)
 
 def get_web_domains_info(env):
 	# load custom settings so we can tell what domains have a redirect or proxy set up on '/',