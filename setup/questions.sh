3a28d1b0733c8440118f2e2a502aeba95b2e2e4a
Joshua Tauberer <jt@occams.info>
2015-06-18 07:35:13 -0400

    showing the Mail-in-a-Box version using git describe was broken since dd6a8d99

--- setup/questions.sh
+++ setup/questions.sh
@@ -233,7 +233,7 @@ fi
 if [ "$PRIVATE_IPV6" != "$PUBLIC_IPV6" ]; then
 	echo "Private IPv6 Address: $PRIVATE_IPV6"
 fi
-if [ -f .git ]; then
+if [ -f /usr/bin/git ] && [ -d .git ]; then
 	echo "Mail-in-a-Box Version: " $(git describe)
 fi
 echo
