4db82d3d09692893925900fd4aa82853a5b2fc27
Michael Kroes <michael.kroes@gmail.com>
2015-10-25 13:19:22 -0400

    Caldav doesnt support sync tokens

--- conf/zpush/backend_caldav.php
+++ conf/zpush/backend_caldav.php
@@ -10,7 +10,7 @@ define('CALDAV_SERVER', 'localhost');
 define('CALDAV_PORT', '443');
 define('CALDAV_PATH', '/caldav/calendars/%u/');
 define('CALDAV_PERSONAL', 'PRINCIPAL');
-define('CALDAV_SUPPORTS_SYNC', true);
+define('CALDAV_SUPPORTS_SYNC', false);
 define('CALDAV_MAX_SYNC_PERIOD', 2147483647);
 
 ?>
