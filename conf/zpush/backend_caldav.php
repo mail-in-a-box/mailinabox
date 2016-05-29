cc7aa347e0f6accf98d6c87cb0a5a746f486769f
aspdye <aspdye@users.noreply.github.com>
2016-05-29 16:12:39 +0200

    Update backend_caldav.php

--- conf/zpush/backend_caldav.php
+++ conf/zpush/backend_caldav.php
@@ -8,7 +8,7 @@
 define('CALDAV_PROTOCOL', 'https');
 define('CALDAV_SERVER', '127.0.0.1');
 define('CALDAV_PORT', '443');
-define('CALDAV_PATH', '/dav/principals/%u/');
+define('CALDAV_PATH', '/dav/principals/users/%u/');
 define('CALDAV_PERSONAL', 'PRINCIPAL');
 define('CALDAV_SUPPORTS_SYNC', false);
 define('CALDAV_MAX_SYNC_PERIOD', 2147483647);
