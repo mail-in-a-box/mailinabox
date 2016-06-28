2bdae89d24ef0cce398f0fb89373bbfaf78c145b
ChiefGyk <alon@ganon.me>
2016-06-28 16:26:52 -0400

    polished and finalized, removed two last lines in blocklist.sh. They were redundant.

--- setup/blocklist.sh
+++ setup/blocklist.sh
@@ -2,11 +2,11 @@
 # Add Blocklist.de malicious IP Addresses to Daily Crontab
 # Also IPtables-persistent to save IP addresses upon reboot
 # Added by Alon "ChiefGyk" Ganon
+# alonganon.info
+# alon@ganon.me
 cp conf/blocklist/sync-fail2ban /etc/cron.daily/sync-fail2ban
 chmod a+x /etc/cron.daily/sync-fail2ban
 time /etc/cron.daily/sync-fail2ban
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt_install iptables-persistent
-iptables-save > /etc/iptables/rules.v4
-ip6tables-save > /etc/iptables/rules.v6
