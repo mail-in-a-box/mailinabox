9d814940f9b6c0502b06047e7971384ecf2fec06
ChiefGyk <alon@ganon.me>
2016-06-28 18:54:47 -0400

    Made script check to see if you are running as root

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 # Add Blocklist.de malicious IP Addresses to Daily Crontab
 # Also IPtables-persistent to save IP addresses upon reboot
 # Added by Alon "ChiefGyk" Ganon
+# Are we running as root?
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root. Please re-run like this:"
+	echo
+	echo "sudo $0"
+	echo
+	exit
+fi
 cp sync-fail2ban /etc/cron.daily/sync-fail2ban
 chmod a+x /etc/cron.daily/sync-fail2ban
 time /etc/cron.daily/sync-fail2ban
