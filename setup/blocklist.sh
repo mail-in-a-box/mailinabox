6c808a56542592eb95ca80225af2411360e05fa9
ChiefGyk <alon@ganon.me>
2016-06-29 07:26:34 -0400

    fixed unneccessary sudo, added part to load global variables.

--- setup/blocklist.sh
+++ setup/blocklist.sh
@@ -4,9 +4,12 @@
 # Added by Alon "ChiefGyk" Ganon
 # alonganon.info
 # alon@ganon.me
+source setup/functions.sh # load our functions
+source /etc/mailinabox.conf # load global vars
+
 cp conf/blocklist/sync-fail2ban /etc/cron.daily/sync-fail2ban
 chmod a+x /etc/cron.daily/sync-fail2ban
 time /etc/cron.daily/sync-fail2ban
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt_install iptables-persistent
