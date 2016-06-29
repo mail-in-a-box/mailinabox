b4154ad1bd1f821c79c6ae677277dc0e4859799a
ChiefGyk <alon@ganon.me>
2016-06-29 07:28:45 -0400

    removed unneeded sudo

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 cp sync-fail2ban /etc/cron.daily/sync-fail2ban
 chmod a+x /etc/cron.daily/sync-fail2ban
 time /etc/cron.daily/sync-fail2ban
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get update
 apt-get install -y iptables-persistent
