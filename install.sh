4f0096c3c26042b0fe2b1b63bdeb0a36538cbde0
ChiefGyk <alon@ganon.me>
2016-06-29 10:18:56 -0400

    somes code fixes and lines added for fixes

--- install.sh
+++ install.sh
@@ -12,14 +12,15 @@ if [[ $EUID -ne 0 ]]; then
 	echo
 	exit
 fi
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get update
 apt-get install -y ipset 
+mkdir /etc/ipset
 ipset create blacklist hash:net
 iptables -I INPUT -m set --match-set blacklist src -j DROP
 cp blacklist /etc/cron.daily/blacklist
 chmod a+x /etc/cron.daily/blacklist
 time /etc/cron.daily/blacklist
-apt-get install -y iptables-persistent
+iptables-save > /etc/iptables.up.rules
+sed -e "\$apost up ipset restore < /etc/ipset/blacklist" /etc/network/interfaces
+sed -e "\$apost up iptables-restore < /etc/iptables.up.rules" /etc/network/interfaces
 echo "Blacklist has been installed. It will run daily automatically."
