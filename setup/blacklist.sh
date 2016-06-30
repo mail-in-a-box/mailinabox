6556da1e65c770806727e261505cb3e0200e1ca6
ChiefGyk <alon@ganon.me>
2016-06-30 08:20:47 -0400

    removed geoblocks, made tor exit node blocking optional during setup via yes/no dialog which will comment out the appropriate line in /etc/cron.daily/blacklist. Rearranged some code, deleted some files and clutter.

--- setup/blacklist.sh
+++ setup/blacklist.sh
@@ -8,14 +8,15 @@
 source setup/functions.sh # load our functions
 source /etc/mailinabox.conf # load global vars
 
+cp conf/blacklist /etc/cron.daily/blacklist
+chmod a+x /etc/cron.daily/blacklist
+source setup/tor.sh
 echo iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean false | debconf-set-selections
 apt_install -y ipset dialog iptables-persistent
 cp conf/iptables-persistent /etc/init.d/iptables-persistent
 ipset create blacklist hash:net
 iptables -I INPUT -m set --match-set blacklist src -j DROP
-cp conf/blacklist/blacklist /etc/cron.daily/blacklist
-chmod a+x /etc/cron.daily/blacklist
 time /etc/cron.daily/blacklist
 source setup/dialog.sh
 /etc/init.d/iptables-persistent save
