69394a563b0af6d77e3d13a855f8efd63a7ab92a
Max Isom <codetheweb@users.noreply.github.com>
2016-09-16 11:56:05 -0500

    Fix domain

--- setup/login-alerts.sh
+++ setup/login-alerts.sh
@@ -1,28 +1,17 @@
-function get_default_hostname {
-	# Guess the machine's hostname. It should be a fully qualified
-	# domain name suitable for DNS. None of these calls may provide
-	# the right value, but it's the best guess we can make.
-	set -- $(hostname --fqdn      2>/dev/null ||
-                 hostname --all-fqdns 2>/dev/null ||
-                 hostname             2>/dev/null)
-	printf '%s\n' "$1" # return this value
-}
+source /etc/mailinabox.conf # load global vars
 
-echo '
+cat <<EOF >> /etc/ssh/login-alert.sh
 #!/bin/sh
-# Change these two lines:
-sender="bot@"
-sender+=get_default_hostname
-recepient="admin@"
-recepient+=get_default_hostname
+sender="bot@PRIMARY_HOSTNAME"
+recepient="admin@$PRIMARY_HOSTNAME"
 
 if [ "$PAM_TYPE" != "close_session" ]; then
-    host="`hostname`"
-    subject="SSH Login: $PAM_USER from $PAM_RHOST on $host"
+    subject="SSH Login: $PAM_USER from $PAM_RHOST"
     # Message to send, e.g. the current environment variables.
     message="If you don't recognize this login, your key or password may be compromised."
     echo "$message" | mailx -r "$sender" -s "$subject" "$recepient"
-fi' > /etc/ssh/login-alert.sh
+fi
+EOF
 
 chmod +x /etc/ssh/login-alert.sh
 
