033ee4956d9ecab6967d64408356fa2c81a71ce3
John Olten <john@olten.xyz>
2017-11-05 18:22:59 +0100

    Add support for DNS wildcard

--- management/dns_update.py
+++ management/dns_update.py
@@ -16,7 +16,7 @@ from utils import shell, load_env_vars_from_file, safe_domain_name, sort_domains
 # This regular expression matches domain names according to RFCs, it also accepts fqdn with an leading dot,
 # as well as underscores which are allowed in domain names but not hostnames (i.e. allowed in
 # DNS but not in URLs), which are common in certain record types like for DKIM.
-DOMAIN_RE = "^(?!\-)(?:[a-zA-Z\d\-_]{0,62}[a-zA-Z\d_]\.){1,126}(?!\d+)[a-zA-Z\d_]{1,63}(\.?)$"
+DOMAIN_RE = "^(?!\-)(?:[a-zA-Z\d\-_]{0,62}[a-zA-Z\d_*]\.){1,126}(?!\d+)[a-zA-Z\d_]{1,63}(\.?)$"
 
 def get_dns_domains(env):
 	# Add all domain names in use by email users and mail aliases and ensure
