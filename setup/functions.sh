21f8e7897dbc7c836583a4f9d8b022113a3da6d8
Jeff Volkenant <jeff@volkenant.net>
2018-12-04 11:08:21 -0800

    Added back trap ERR, sourced apps don't always show the correct application

--- setup/functions.sh
+++ setup/functions.sh
@@ -4,6 +4,8 @@
 # -o pipefail: don't ignore errors in the non-last command in a pipeline 
 set -euo pipefail
 trap 'rc=$?; echo "$0: Error on line $LINENO running $BASH_COMMAND which had the following exit code $rc" 1>&2' EXIT
+trap 'rc=$?; echo "$0: Error on line $LINENO running $BASH_COMMAND which had the following exit code $rc" 1>&2' ERR
+/bin/false
 function hide_output {
 	# This function hides the output of a command unless the command fails
 	# and returns a non-zero exit code.
