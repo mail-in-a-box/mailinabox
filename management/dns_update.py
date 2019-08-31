mail-in-a-box/mailinabox

commit c7377e602d06a792b97a5a1047bbdeb32bdd377a

make it possible to use subnet addresses for axfr (#1616)
it is sometimes needed to be able to set axfr to more than just one ip address. This can be done with multiple xfr: in  the secondary dns input but if you need to add an entire subnet segment (xxx.xxx.xxx.0/yy) then it will not work.
With this patch it is now possible to use a subnet as input for xfr the same way as if it was an ip address.

management/dns_update.py
@@ -903,8 +903,12 @@ def set_secondary_dns(hostnames, env):
 			else:
 				# Validate IP address.
 				try:
-					v = ipaddress.ip_address(item[4:]) # raises a ValueError if there's a problem
-					if not isinstance(v, ipaddress.IPv4Address): raise ValueError("That's an IPv6 address.")
+                    if "/" in item[4:]:
+                        v = ipaddress.ip_network(item[4:] # raises a ValueError if there's a problem
+                        if not isinstance(v, ipaddress.IPv4Network): raise ValueError("That's an IPv6 subnet.")
+                    else:
+					    v = ipaddress.ip_address(item[4:]) # raises a ValueError if there's a problem
+					    if not isinstance(v, ipaddress.IPv4Address): raise ValueError("That's an IPv6 address.")
 				except ValueError:
 					raise ValueError("'%s' is not an IPv4 address." % item[4:])
 

commit 3ff9817325d6d113c03f2d8f3b1eef9623b07e87

document the xfr: CIDR notation, fix spaces vs tabs and syntax error, broken by c7377e602d06a792b97a5a1047bbdeb32bdd377a, #1616

management/dns_update.py
@@ -903,14 +903,14 @@ def set_secondary_dns(hostnames, env):
 			else:
 				# Validate IP address.
 				try:
-                    if "/" in item[4:]:
-                        v = ipaddress.ip_network(item[4:] # raises a ValueError if there's a problem
-                        if not isinstance(v, ipaddress.IPv4Network): raise ValueError("That's an IPv6 subnet.")
-                    else:
-					    v = ipaddress.ip_address(item[4:]) # raises a ValueError if there's a problem
-					    if not isinstance(v, ipaddress.IPv4Address): raise ValueError("That's an IPv6 address.")
+					if "/" in item[4:]:
+						v = ipaddress.ip_network(item[4:]) # raises a ValueError if there's a problem
+						if not isinstance(v, ipaddress.IPv4Network): raise ValueError("That's an IPv6 subnet.")
+					else:
+						v = ipaddress.ip_address(item[4:]) # raises a ValueError if there's a problem
+						if not isinstance(v, ipaddress.IPv4Address): raise ValueError("That's an IPv6 address.")
 				except ValueError:
-					raise ValueError("'%s' is not an IPv4 address." % item[4:])
+					raise ValueError("'%s' is not an IPv4 address or subnet." % item[4:])
 
 		# Set.
 		set_custom_dns_record("_secondary_nameserver", "A", " ".join(hostnames), "set", env)

management/templates/custom-dns.html
@@ -90,7 +90,7 @@
     <div class="col-sm-offset-1 col-sm-11">
       <p class="small">
         Multiple secondary servers can be separated with commas or spaces (i.e., <code>ns2.hostingcompany.com ns3.hostingcompany.com</code>). 
-        To enable zone transfers to additional servers without listing them as secondary nameservers, add <code>xfr:IPADDRESS</code>.
+        To enable zone transfers to additional servers without listing them as secondary nameservers, add an IP address or subnet using <code>xfr:10.20.30.40</code> or <code>xfr:10.20.30.40/24</code>.
       </p>
       <p id="secondarydns-clear-instructions" style="display: none" class="small">
         Clear the input field above and click Update to use this machine itself as secondary DNS, which is the default/normal setup.