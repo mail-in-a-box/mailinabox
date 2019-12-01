ad9d7326083090f0c2a57ed727b2d82722190264
Vasek Sraier <vakabus@users.noreply.github.com>
2019-12-01 22:24:38 +0100

    OpenDKIM canonicalization changed to relaxed for mail headers (#1620)
    Because Mailman reformats headers it breaks DKIM signatures. SPF also does
    not apply in mailing lists. This together causes DMARC to fail and mark the
    email as invalid. This fixes DKIM signatures for Mailman-based mailing lists
    and makes sure DMARC test is passed.

--- setup/dkim.sh
+++ setup/dkim.sh
@@ -31,6 +31,7 @@ if grep -q "ExternalIgnoreList" /etc/opendkim.conf; then
 else
 	# Add various configuration options to the end of `opendkim.conf`.
 	cat >> /etc/opendkim.conf << EOF;
+Canonicalization		relaxed/simple
 MinimumKeyBits          1024
 ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
 InternalHosts           refile:/etc/opendkim/TrustedHosts
