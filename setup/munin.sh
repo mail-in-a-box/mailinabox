960b5d5bbddd469f77f91540b35980ce026ff46b
Carl Reinke <carlreinke@users.noreply.github.com>
2019-12-01 14:21:38 -0700

    Don't use ifquery to check interface state since it is no longer installed (#1689)

--- setup/munin.sh
+++ setup/munin.sh
@@ -53,7 +53,7 @@ find /etc/munin/plugins/ -lname /usr/share/munin/plugins/ntp_ -print0 | xargs -0
 # Deactivate monitoring of network interfaces that are not up. Otherwise we can get a lot of empty charts.
 for f in $(find /etc/munin/plugins/ \( -lname /usr/share/munin/plugins/if_ -o -lname /usr/share/munin/plugins/if_err_ -o -lname /usr/share/munin/plugins/bonding_err_ \)); do
 	IF=$(echo $f | sed s/.*_//);
-	if ! ifquery $IF >/dev/null 2>/dev/null; then
+	if ! grep -qFx up /sys/class/net/$IF/operstate 2>/dev/null; then
 		rm $f;
 	fi;
 done
