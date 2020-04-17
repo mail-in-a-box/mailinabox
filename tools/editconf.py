c96c04b39d273e05883e826692b605e10e27c840
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-04-17 22:21:06 +0100

    I screwed up

--- tools/editconf.py
+++ tools/editconf.py
@@ -4,4 +4,4 @@ from os import system
 from sys import argv
 
 # Pass control to the actual script
-system(f"management/editconf.py {' '.join(str(x) for x in argv[:1])}")
+system(f"management/editconf.py {' '.join(str(x) for x in argv[1:])}")
