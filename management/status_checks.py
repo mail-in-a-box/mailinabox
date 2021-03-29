55e312a8bba3d0e63d93bb3fab7970a077585f36
hala alajlan <halalajlan@gmail.com>
2021-03-29 15:31:54 +0300

    handle query dns timeout

--- management/status_checks.py
+++ management/status_checks.py
@@ -621,6 +621,8 @@ def check_mail_domain(domain, env, output):
 
 	if mx is None:
 		mxhost = None
+	elif mx == "[timeout]":
+        mxhost = None
 	else:
 		# query_dns returns a semicolon-delimited list
 		# of priority-host pairs.
