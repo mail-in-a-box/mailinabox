26609c422358b96ccf96eabc7c211c6ae687f663
downtownallday <downtownallday@gmail.com>
2021-04-10 16:49:46 -0400

    Fix cell alignment

--- management/reporting/ui/chart-table.js
+++ management/reporting/ui/chart-table.js
@@ -38,7 +38,7 @@ export default Vue.component('chart-table', {
             if (this.items.length == 0) {
                 return [{
                     key: 'no data',
-                    thClass: 'text-nowrap'
+                    thClass: 'text-nowrap align-top'
                 }];
             }
             return this.fields;
