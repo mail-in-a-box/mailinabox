b2d966f8e4ed3a62d5721350fef2099484a7905a
KiekerJan <github@kiekerjan.isdronken.nl>
2021-04-29 23:03:09 +0200

    add rootkit detectors

--- setup/additionals.sh
+++ setup/additionals.sh
@@ -2,7 +2,7 @@ source /etc/mailinabox.conf
 source setup/functions.sh
 
 # Add additional packages
-apt_install pflogsumm
+apt_install pflogsumm rkhunter chkrootkit
 
 # Cleanup old spam and trash email
 hide_output install -m 755 conf/cron/miab_clean_mail /etc/cron.weekly/
@@ -25,3 +25,28 @@ hide_output systemctl restart systemd-journald.service
 cat > /root/.forward << EOF;
 administrator@$PRIMARY_HOSTNAME
 EOF
+
+# Install fake mail script
+if [ ! -f /usr/local/bin/mail ]; then
+        hide_output install -m 755 tools/fake_mail /usr/local/bin
+        mv -f /usr/local/bin/fake_mail /usr/local/bin/mail
+fi
+
+tools/editconf.py /etc/rkhunter.conf \
+        UPDATE_MIRRORS=1 \
+        MIRRORS_MODE=0 \
+        WEB_CMD='""' \
+        MAIL-ON-WARNING=root \
+        ALLOWHIDDENDIR=/etc/.java
+
+tools/editconf.py /etc/default/rkhunter \
+        CRON_DAILY_RUN='"true"' \
+        CRON_DB_UPDATE='"true"' \
+        APT_AUTOGEN='"true"'
+
+tools/editconf.py /etc/chkrootkit.conf \
+        RUN_DAILY='"true"'
+
+# Should be last
+rkhunter --propupd
+
