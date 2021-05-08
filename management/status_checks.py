603cf995003394cbc14e0d025867dc3608f0ddb9
Michael Kroes <michael@kroes.email>
2021-05-08 15:12:45 +0200

    Fixup tabs

--- management/status_checks.py
+++ management/status_checks.py
@@ -665,7 +665,7 @@ def check_mail_domain(domain, env, output):
 	if mx is None:
 		mxhost = None
 	elif mx == "[timeout]":
-        mxhost = None
+		mxhost = None
 	else:
 		# query_dns returns a semicolon-delimited list
 		# of priority-host pairs.
