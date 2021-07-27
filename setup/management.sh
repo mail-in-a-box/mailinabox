41011f8639b4c2854c5f9bf654db4878cc821b4a
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-07-27 22:21:15 +0800

    Add expiringdict to setup/management.sh install

--- setup/management.sh
+++ setup/management.sh
@@ -51,7 +51,7 @@ hide_output $venv/bin/pip install --upgrade \
 	rtyaml "email_validator>=1.0.0" "exclusiveprocess" \
 	flask dnspython python-dateutil \
   qrcode[pil] pyotp \
-	"idna>=2.0.0" "cryptography==2.2.2" boto psutil postfix-mta-sts-resolver b2sdk
+	"idna>=2.0.0" "cryptography==2.2.2" boto psutil postfix-mta-sts-resolver b2sdk expiringdict
 
 # CONFIGURATION
 
