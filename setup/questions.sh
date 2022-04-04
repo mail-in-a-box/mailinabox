abeaa4c501d1cc085d842f0e363e8e119bb185ac
kaibae19 <99116238+kaibae19@users.noreply.github.com>
2022-04-03 18:24:28 -0700

    Update questions.sh

--- setup/questions.sh
+++ setup/questions.sh
@@ -94,6 +94,8 @@ if [ -z "${DAV_HOSTNAME:-}" ]; then
     DAV_HOSTNAME
 fi
 
+echo "Passed DAV Input"
+
 # If the machine is behind a NAT, inside a VM, etc., it may not know
 # its IP address on the public network / the Internet. Ask the Internet
 # and possibly confirm with user.
