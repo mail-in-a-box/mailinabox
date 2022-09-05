1d86202b6fe51b5edfdb297ad9f0e01ec1fa00e6
Steve Hay <hay.steve@gmail.com>
2022-09-05 19:36:47 -0400

    trying 1 gunicorn worker

--- management/wsgi.py
+++ management/wsgi.py
@@ -1,15 +1,6 @@
 from daemon import app
 import auth, utils
 
-from werkzeug.middleware.proxy_fix import ProxyFix
-
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
-
-env = utils.load_environment()
-auth_service = auth.AuthService()
-
 app.logger.addHandler(utils.create_syslog_handler())
 
 if __name__ == "__main__":
--- setup/management.sh
+++ setup/management.sh
@@ -100,7 +100,7 @@ export LC_TYPE=en_US.UTF-8
 
 source $venv/bin/activate
 export PYTHONPATH=$(pwd)
-exec gunicorn -b localhost:10222 -w 2 wsgi:app
+exec gunicorn -b localhost:10222 -w 1 wsgi:app
 EOF
 chmod +x $inst_dir/start
 cp --remove-destination conf/mailinabox.service /lib/systemd/system/mailinabox.service # target was previously a symlink so remove it first
