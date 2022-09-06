4a4ad258c69a8afeeb976e8bb647b3cb61719d2c
downtownallday <downtownallday@gmail.com>
2022-09-05 20:35:04 -0400

    keep trying to umount 3 times with 10 seconds between tries

--- ehdd/shutdown.sh
+++ ehdd/shutdown.sh
@@ -14,10 +14,12 @@ fi
 
 ehdd/umount.sh
 code=$?
-if [ $code -eq 2 ]; then
+tries=1
+while [ $code -eq 2 -a $tries -le 3 ]; do
     echo "Trying again in 10 seconds..."
     sleep 10
     ehdd/umount.sh
     code=$?
-fi
+    let tries+=1
+done
 exit $code
