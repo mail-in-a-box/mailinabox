809a75765e7b44e00ddb70bfc0d3a40f587972d0
downtownallday <downtownallday@gmail.com>
2022-09-18 15:44:01 -0400

    replace FLASK_ENV, which has been deprecated, with FLASK_DEBUG

--- tests/bin/m-debug.sh
+++ tests/bin/m-debug.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")/../../management" || exit 1
 systemctl stop mailinabox
 source /usr/local/lib/mailinabox/env/bin/activate
 export DEBUG=1
-export FLASK_ENV=development
+export FLASK_DEBUG=1
 if ! systemctl is-active --quiet miabldap-capture; then
     export CAPTURE_STORAGE_ROOT=/mailinabox/management/reporting/capture/tests
 fi
