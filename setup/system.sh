3c4460431686142fbc9d8da98a829e58a4e277ac
Joshua Tauberer <jt@occams.info>
2022-09-24 10:08:57 -0400

    Install 'file' package
    The command is used in mailinabox-postgrey-whitelist. Reported missing (on systems that don't install it by default) in #2083.

--- setup/system.sh
+++ setup/system.sh
@@ -137,7 +137,7 @@ apt_get_quiet autoremove
 
 echo Installing system packages...
 apt_install python3 python3-dev python3-pip python3-setuptools \
-	netcat-openbsd wget curl git sudo coreutils bc \
+	netcat-openbsd wget curl git sudo coreutils bc file \
 	pollinate openssh-client unzip \
 	unattended-upgrades cron ntp fail2ban rsyslog
 
