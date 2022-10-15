4914b0c4237c61501a710667671eeffa2ea8770a
KiekerJan <github@kiekerjan.isdronken.nl>
2022-10-15 07:59:25 +0200

    add logging

--- management/wsgi.py
+++ management/wsgi.py
@@ -1,7 +1,11 @@
 from daemon import app
-import auth, utils
+import auth, utils, logging
 
 app.logger.addHandler(utils.create_syslog_handler())
 
+logging_level = logging.DEBUG
+logging.basicConfig(level=logging_level, format='MiaB %(levelname)s:%(module)s.%(funcName)s %(message)s')
+logging.info('Logging level set to %s', logging.getLevelName(logging_level))
+        
 if __name__ == "__main__":
     app.run(port=10222)
