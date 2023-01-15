57047d96e95c7c1f71da3b80f78d487f1c0acb61
Hugh Secker-Walker <hsw@hodain.net>
2023-01-15 08:25:36 -0500

    chore(setup): Update obsolete chown group syntax (#2202)
    Co-authored-by: Hugh Secker-Walker <hsw+miac@hodain.net>

--- tools/owncloud-restore.sh
+++ tools/owncloud-restore.sh
@@ -40,8 +40,8 @@ cp "$1/owncloud.db" $STORAGE_ROOT/owncloud/
 cp "$1/config.php" $STORAGE_ROOT/owncloud/
 
 ln -sf $STORAGE_ROOT/owncloud/config.php /usr/local/lib/owncloud/config/config.php
-chown -f -R www-data.www-data $STORAGE_ROOT/owncloud /usr/local/lib/owncloud
-chown www-data.www-data $STORAGE_ROOT/owncloud/config.php
+chown -f -R www-data:www-data $STORAGE_ROOT/owncloud /usr/local/lib/owncloud
+chown www-data:www-data $STORAGE_ROOT/owncloud/config.php
 
 sudo -u www-data php$PHP_VER /usr/local/lib/owncloud/occ maintenance:mode --off
 
