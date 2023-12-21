7655a7688c955f711c404f4fec70aa561f848ff6
Teal Dulcet <tdulcet@pdx.edu>
2023-12-21 06:15:50 -0800

    Fixed SC2006: Use $(...) notation instead of legacy backticks `...`.

--- tools/archive_conf_files.sh
+++ tools/archive_conf_files.sh
@@ -1,6 +1,6 @@
 # Use this script to make an archive of the contents of all
 # of the configuration files we edit with editconf.py.
-for fn in `grep -hr editconf.py setup | sed "s/tools\/editconf.py //" | sed "s/ .*//" | sort | uniq`; do
+for fn in $(grep -hr editconf.py setup | sed "s/tools\/editconf.py //" | sed "s/ .*//" | sort | uniq); do
 	echo ======================================================================
 	echo $fn
 	echo ======================================================================
