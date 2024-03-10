4ad679da477cfaa3ef72d18108617e6352e0fbd4
John James Jacoby <johnjamesjacoby@me.com>
2024-03-10 06:16:03 -0500

    Issue-2354: Silence "wal" output on setup (#2356)
    Silence "wal" output from RoundCube Sqlite customization, inside of webmail.sh.
    
    Co-authored-by: solomon-s-b
    
    Fixes #2354.

--- setup/webmail.sh
+++ setup/webmail.sh
@@ -217,7 +217,7 @@ sed -i.miabold 's/^[^#]\+.\+PRAGMA journal_mode = WAL.\+$/#&/' \
 # Because Roundcube wants to set the PRAGMA we just deleted from the source, we apply it here
 # to the roundcube database (see https://github.com/roundcube/roundcubemail/issues/8035)
 # Database should exist, created by migration script
-sqlite3 $STORAGE_ROOT/mail/roundcube/roundcube.sqlite 'PRAGMA journal_mode=WAL;'
+sqlite3 $STORAGE_ROOT/mail/roundcube/roundcube.sqlite 'PRAGMA journal_mode=WAL;' | 2>&1
 
 # Enable PHP modules.
 phpenmod -v $PHP_VER imap
