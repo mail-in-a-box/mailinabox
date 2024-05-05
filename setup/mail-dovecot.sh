0cd8e4db62b237c170f4688b7d07ef2f0cbd03a5
Chad Furman <chad@chadfurman.com>
2024-05-04 22:22:47 -0400

    fixing imap sed script

--- setup/mail-dovecot.sh
+++ setup/mail-dovecot.sh
@@ -69,7 +69,7 @@ tools/editconf.py /etc/dovecot/conf.d/10-mail.conf \
 cp conf/dovecot/conf.d/15-mailboxes.conf /etc/dovecot/conf.d/
 sed -i "s/#mail_plugins =\(.*\)/mail_plugins =\1 \$mail_plugins quota/" /etc/dovecot/conf.d/10-mail.conf
 if ! grep -q "mail_plugins.* imap_quota" /etc/dovecot/conf.d/20-imap.conf; then
-    sed -i "s/mail_plugins =\(.*\)/mail_plugins =\1 imap_quota/" /etc/dovecot/conf.d/20-imap.conf
+  sed -i "s/\(mail_plugins =.*\)/\1\n  mail_plugins = \$mail_plugins imap_quota/" /etc/dovecot/conf.d/20-imap.conf
 fi
 
 # configure stuff for quota support
