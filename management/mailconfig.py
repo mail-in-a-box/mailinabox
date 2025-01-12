daf6d700737d44f16aff97eb89bc648c4b7ac546
Teal Dulcet <tdulcet@pdx.edu>
2025-01-12 07:12:44 -0800

    Fixed ARG005 (unused-lambda-argument): Unused lambda argument: `alias`

--- management/mailconfig.py
+++ management/mailconfig.py
@@ -255,7 +255,7 @@ def get_domain(emailaddr, as_unicode=True):
 			pass
 	return ret
 
-def get_mail_domains(env, filter_aliases=lambda alias : True, users_only=False):
+def get_mail_domains(env, filter_aliases=lambda _alias : True, users_only=False):
 	# Returns the domain names (IDNA-encoded) of all of the email addresses
 	# configured on the system. If users_only is True, only return domains
 	# with email addresses that correspond to user accounts. Exclude Unicode
